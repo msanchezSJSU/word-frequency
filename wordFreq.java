cd3f4891a75c79e536f0ae2bd2828c32f0924cb5
Marko Sanchez <marko.sanchez@sjsu.edu>
2026-02-12 15:36:25 -0800

    Added basic class methods and populated array with class objects

--- wordFreq.java
+++ wordFreq.java
@@ -7,7 +7,34 @@ public class wordFreq {
         this.frequency = frequency;
     }
 
+    public String getWord(){
+        return this.word;
+    }
+
+    public int getFreq(){
+        return this.frequency;
+    }
+
+    public wordFreq compare(wordFreq wf1, wordFreq wf2){
+        if (wf1.getFreq() > wf2.getFreq()){
+            return wf1;
+        } else {
+            return wf2;
+        }
+    }
+
     public static void main(String[] args) {
-        wordFreq[] wf = new wordFreq[10];
+        wordFreq[] wf = {
+            new wordFreq("happy",400),
+            new wordFreq("satisfied", 100),
+            new wordFreq("neutral", 300),
+            new wordFreq("would buy again", 200),
+            new wordFreq("terrible",160),
+            new wordFreq("inconvenient", 900),
+            new wordFreq("difficult to use", 100),
+            new wordFreq("easy to use", 140),
+            new wordFreq("would recommend to friends", 800),
+            new wordFreq("visit the store", 700),
+        };
     }
 }
