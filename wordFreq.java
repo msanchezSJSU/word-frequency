cc06094a60859e5719404e892acc08e5a408f758
Marko Sanchez <marko.sanchez@sjsu.edu>
2026-02-12 15:25:23 -0800

    Initial class setup

---
+++ wordFreq.java
@@ -0,0 +1,13 @@
+public class wordFreq {
+    String word;
+    int frequency;
+
+    public wordFreq(String word, int frequency){
+        this.word = word;
+        this.frequency = frequency;
+    }
+
+    public static void main(String[] args) {
+        wordFreq[] wf = new wordFreq[10];
+    }
+}
